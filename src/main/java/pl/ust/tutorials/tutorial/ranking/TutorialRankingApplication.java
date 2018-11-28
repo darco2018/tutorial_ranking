3855b3dfeeb631891f6013dfe091fd635927a9cc
darco2018 <newsecond@outlook.com>
2018-11-28 07:51:31 +0100

    Initial commit

---
+++ src/main/java/pl/ust/tutorials/tutorial/ranking/TutorialRankingApplication.java
@@ -0,0 +1,12 @@
+package pl.ust.tutorials.tutorial.ranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialRankingApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/ust/tutorials/tutorial/ranking/TutorialRankingApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.ust.tutorials.tutorial.ranking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TutorialRankingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
