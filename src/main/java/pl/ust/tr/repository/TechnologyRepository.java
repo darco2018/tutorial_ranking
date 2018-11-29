8d7aef997cc9c6b421e97325eda0670bda0db5e9
darco2018 <newsecond@outlook.com>
2018-11-29 19:27:47 +0100

    Add Paging and sorting

--- src/main/java/pl/ust/tr/repository/TechnologyRepository.java
+++ src/main/java/pl/ust/tr/repository/TechnologyRepository.java
@@ -1,9 +1,10 @@
 package pl.ust.tr.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import pl.ust.tr.domain.Technology;
 
 public interface TechnologyRepository extends CrudRepository<Technology, String> {
 
-    Technology findByName(String name);
+    Technology findByName(@Param("name") String name);
 }
--- src/main/java/pl/ust/tr/repository/TutorialRepository.java
+++ src/main/java/pl/ust/tr/repository/TutorialRepository.java
@@ -1,7 +1,18 @@
 package pl.ust.tr.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import pl.ust.tr.domain.Tutorial;
 
-public interface TutorialRepository extends CrudRepository<Tutorial, Integer> {
+import java.util.List;
+
+//public interface TutorialRepository extends CrudRepository<Tutorial, Integer> {
+
+public interface TutorialRepository extends PagingAndSortingRepository<Tutorial, Integer> {
+
+    //List<Tutorial> findByTechnologyCode(@Param("code") String code);
+    Page<Tutorial> findByTechnologyCode(@Param("code") String code, Pageable pageable);
 }
