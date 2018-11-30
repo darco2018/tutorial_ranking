c84e9df2175b66abac7f8ab59e82d329f8e3373c
darco2018 <newsecond@outlook.com>
2018-11-30 09:43:01 +0100

    Restrict access to rest resource

--- src/main/java/pl/ust/tr/repository/TechnologyRepository.java
+++ src/main/java/pl/ust/tr/repository/TechnologyRepository.java
@@ -2,9 +2,36 @@ package pl.ust.tr.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import pl.ust.tr.domain.Technology;
 
+//@RepositoryRestResource(collectionResourceRel = "technology-tool", path = "technology-tool")
 public interface TechnologyRepository extends CrudRepository<Technology, String> {
 
     Technology findByName(@Param("name") String name);
+
+    @Override
+    @RestResource(exported=false)
+    <S extends Technology> S save(S s);
+
+    @Override
+    @RestResource(exported=false)
+    <S extends Technology> Iterable<S> save(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported=false)
+    void delete(String s);
+
+    @Override
+    @RestResource(exported=false)
+    void delete(Technology technology);
+
+    @Override
+    @RestResource(exported=false)
+    void delete(Iterable<? extends Technology> iterable);
+
+    @Override
+    @RestResource(exported=false)
+    void deleteAll();
 }
--- src/main/java/pl/ust/tr/repository/TutorialRepository.java
+++ src/main/java/pl/ust/tr/repository/TutorialRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import pl.ust.tr.domain.Tutorial;
 
 import java.util.List;
@@ -15,4 +16,28 @@ public interface TutorialRepository extends PagingAndSortingRepository<Tutorial,
 
     //List<Tutorial> findByTechnologyCode(@Param("code") String code);
     Page<Tutorial> findByTechnologyCode(@Param("code") String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported=false)
+    <S extends Tutorial> S save(S s);
+
+    @Override
+    @RestResource(exported=false)
+    <S extends Tutorial> Iterable<S> save(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported=false)
+    void delete(Integer integer);
+
+    @Override
+    @RestResource(exported=false)
+    void delete(Tutorial tutorial);
+
+    @Override
+    @RestResource(exported=false)
+    void delete(Iterable<? extends Tutorial> iterable);
+
+    @Override
+    @RestResource(exported=false)
+    void deleteAll();
 }
