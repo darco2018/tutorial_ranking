1f51323108821eacd66227c1fcd12b195f667b79
darco2018 <newsecond@outlook.com>
2018-12-17 10:31:16 +0100

    Move Dockerfile to ./docker

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 source ./scripts/config.sh
 
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Starting the script to compile, skip tests, build the JAR, create an image from Dockerfile-2 and run the app in the Docker container, link to the database"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Starting the script to compile, skip tests, build the JAR, create an image from Dockerfile and run the app in the Docker container, link to the database"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Building the project with Maven Wrapper (skipping tests)... >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 # always adds compiled changes
 ./mvnw clean package -DskipTests
@@ -10,7 +10,7 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Building the project with Maven Wrapper (sk
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Building ${app_image_name} from  Dockefile... >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 # the whole image is always created anew, rather than adding new changes
-docker build -f Dockerfile-2 -t ${app_image_name} ./
+docker build -f ./docker/Dockerfile -t ${app_image_name} ./
 
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Starting the script link-to-db.sh ... >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
