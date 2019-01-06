61c59921433a699fa2df5be39830c401f4172e76
darco2018 <newsecond@outlook.com>
2019-01-06 13:07:55 +0100

    Renaming in Dockerfile & scripts

--- scripts/link-to-db.sh
+++ scripts/link-to-db.sh
@@ -7,8 +7,8 @@ echo "docker run \
      --name ${app_container} \
      -p ${serverport}:8080 \
      --env-file=docker/docker_env \
-     -v LOG_DIR":"LOG_DIR \
-     -v MIGRATION_DIR":"MIGRATION_DIR \
+     -v ${LOG_ABS_PATH}${app_name}":"${LOG_ABS_PATH}${app_name} \
+     -v ${MIGRATION_ABS_PATH}${app_name}":"${MIGRATION_ABS_PATH}${app_name} \
      -v /etc/timezone:/etc/timezone:ro \
      --link ${db_container}:mysql \
      -it -d --rm ${app_image}"
@@ -17,8 +17,8 @@ echo "docker run \
      --name ${app_container} \
      -p ${serverport}:8080 \
      --env-file=docker/docker_env \
-     -v ${LOG_PATH}${app_name}":"${LOG_PATH}${app_name} \
-     -v ${MIGRATION_PATH}${app_name}":"${MIGRATION_PATH}${app_name} \
+     -v ${LOG_ABS_PATH}${app_name}":"${LOG_ABS_PATH}${app_name} \
+     -v ${MIGRATION_ABS_PATH}${app_name}":"${MIGRATION_ABS_PATH}${app_name} \
      -v /etc/timezone:/etc/timezone:ro \
      --link ${db_container}:mysql \
      -it -d --rm ${app_image}
